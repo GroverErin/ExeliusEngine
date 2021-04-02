7ea03e47f2b6530eb6081c0ce087152de3615aa6
GroverErin <eringrover95@gmail.com>
2021-04-02 16:01:00 -0700

    Readded ThirdParty.

---
+++ Tools/ThirdParty/SFML/SFML_premake5.lua
@@ -0,0 +1,227 @@
+printf("Including sfml_premake5.lua - This should only appear once.")
+------------------------------------------------------------------------------------------------------
+--[[
+    Exelius Game Engine - Game Development Tool
+    Copyright (c) 2020 Exelius Entertainment
+    Created By: Erin "Ryukana" Grover
+
+    This Engine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    License for more details. You should have received a copy of the
+    License along with this Engine.
+--]]
+------------------------------------------------------------------------------------------------------
+
+local dependencies = require("Tools/ThirdParty/DependencyManager")
+
+local sfml = 
+{
+	location = "SFML"
+}
+
+function sfml.Include(rootDir)
+	includedirs
+	{
+		rootDir .. [[include/]]
+	}
+
+	--------------------------------------------------------------------------------------------------
+	-- Windows Library Directories
+	--------------------------------------------------------------------------------------------------
+
+	filter{"configurations:Debug32", "system:windows"}
+		libdirs
+		{
+			rootDir .. [[lib/Win32/Debug/]],
+			rootDir .. [[extlibs/libs-msvc/x86/]]
+		}
+
+	filter{"configurations:Test32", "system:windows"}
+		libdirs
+		{
+			rootDir .. [[lib/Win32/RelWithDebInfo/]],
+			rootDir .. [[extlibs/libs-msvc/x86/]]
+		}
+
+	filter{"configurations:Release32", "system:windows"}
+		libdirs
+		{
+			rootDir .. [[lib/Win32/MinSizeRel/]],
+			rootDir .. [[extlibs/libs-msvc/x86/]]
+		}
+
+	filter{"configurations:Debug64", "system:windows"}
+		libdirs
+		{
+			rootDir .. [[lib/Win64/Debug/]],
+			rootDir .. [[extlibs/libs-msvc/x64/]]
+		}
+
+	filter{"configurations:Test64", "system:windows"}
+		libdirs
+		{
+			rootDir .. [[lib/Win64/RelWithDebInfo/]],
+			rootDir .. [[extlibs/libs-msvc/x64/]]
+		}
+
+	filter{"configurations:Release64", "system:windows"}
+		libdirs
+		{
+			rootDir .. [[lib/Win64/MinSizeRel/]],
+			rootDir .. [[extlibs/libs-msvc/x64/]]
+		}
+
+	--------------------------------------------------------------------------------------------------
+	-- Linux Library Directories
+	--------------------------------------------------------------------------------------------------
+
+	filter{"configurations:Debug32", "system:linux"}
+		libdirs
+		{
+			rootDir .. [[lib/Lin32/Debug/]],
+		}
+
+	filter{"configurations:Test32", "system:linux"}
+		libdirs
+		{
+			rootDir .. [[lib/Lin32/Release/]]
+		}
+
+	filter{"configurations:Release32", "system:linux"}
+		libdirs
+		{
+			rootDir .. [[lib/Lin32/Release/]]
+		}
+
+	filter{"configurations:Debug64", "system:linux"}
+		libdirs
+		{
+			rootDir .. [[lib/Lin64/Debug/]],
+		}
+
+	filter{"configurations:Test64", "system:linux"}
+		libdirs
+		{
+			rootDir .. [[lib/Lin64/Release/]]
+		}
+
+	filter{"configurations:Release64", "system:linux"}
+		libdirs
+		{
+			rootDir .. [[lib/Lin64/Release/]]
+		}
+
+	filter("system:windows")
+		defines 
+		{
+			"SFML_STATIC",
+			"EXELIUS_RENDERER=SFML_RENDERER"
+		}
+
+	filter("system:linux")
+		defines 
+		{
+			"LINUX",
+			"EXELIUS_RENDERER=SFML_RENDERER"
+		}
+
+	filter{}
+end
+
+function sfml.Link(rootdir, exeliusLibDir)
+
+	filter{"configurations:Debug*", "system:windows"}
+		links
+		{
+			"sfml-audio-s-d",
+			"sfml-graphics-s-d",
+			"sfml-network-s-d",
+			"sfml-system-s-d",
+			"sfml-window-s-d",
+			"flac",
+			"freetype",
+			"ogg",
+			"openal32",
+			"vorbis",
+			"vorbisenc",
+			"vorbisfile",
+			"opengl32",
+			"gdi32",
+			"winmm"
+		}
+
+	filter{"configurations:Test*", "system:windows"}
+		links 
+		{
+			"sfml-audio-s",
+			"sfml-graphics-s",
+			"sfml-network-s",
+			"sfml-system-s",
+			"sfml-window-s",
+			"flac",
+			"freetype",
+			"ogg",
+			"openal32",
+			"vorbis",
+			"vorbisenc",
+			"vorbisfile",
+			"opengl32",
+			"gdi32",
+			"winmm"
+		}
+
+	filter{"configurations:Release*", "system:windows"}
+		links 
+		{
+			"sfml-audio-s",
+			"sfml-graphics-s",
+			"sfml-network-s",
+			"sfml-system-s",
+			"sfml-window-s",
+			"flac",
+			"freetype",
+			"ogg",
+			"openal32",
+			"vorbis",
+			"vorbisenc",
+			"vorbisfile",
+			"opengl32",
+			"gdi32",
+			"winmm"
+		}
+
+	filter{"configurations:Debug*", "system:linux"}
+		links
+		{
+			"sfml-audio-d",
+			"sfml-graphics-d",
+			"sfml-network-d",
+			"sfml-system-d",
+			"sfml-window-d"
+		}
+
+	filter{"configurations:Test*", "system:linux"}
+		links 
+		{
+			"sfml-audio",
+			"sfml-graphics",
+			"sfml-network",
+			"sfml-system",
+			"sfml-window"
+		}
+
+	filter{"configurations:Release*", "system:linux"}
+		links 
+		{
+			"sfml-audio",
+			"sfml-graphics",
+			"sfml-network",
+			"sfml-system",
+			"sfml-window"
+		}
+
+	filter {}
+end
+
+dependencies.Add("SFML", sfml)
---
+++ Tools/ThirdParty/libsodium/libsodium_premake5.lua
@@ -0,0 +1,129 @@
+printf("Including libsodium_premake5.lua - This should only appear once.")
+------------------------------------------------------------------------------------------------------
+--[[
+    Exelius Game Engine - Game Development Tool
+    Copyright (c) 2020 Exelius Entertainment
+    Created By: Erin "Ryukana" Grover
+
+    This Engine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    License for more details. You should have received a copy of the
+    License along with this Engine.
+--]]
+------------------------------------------------------------------------------------------------------
+
+local dependencies = require("Tools/ThirdParty/DependencyManager")
+
+local libsodium = 
+{
+	location = "libsodium"
+}
+
+function libsodium.Include(rootDir)
+	includedirs
+	{
+		rootDir .. [[include/]]
+	}
+
+    --------------------------------------------------------------------------------------------------
+	-- Windows Library Directories
+	--------------------------------------------------------------------------------------------------
+
+    filter{"configurations:Debug32", "system:windows"}
+        libdirs
+        {
+            rootDir .. [[lib/Win32/Debug/]],
+        }
+
+    filter{"configurations:Test32", "system:windows"}
+        libdirs
+        {
+            rootDir .. [[lib/Win32/Release/]]
+        }
+
+    filter{"configurations:Release32", "system:windows"}
+        libdirs
+        {
+            rootDir .. [[lib/Win32/Release/]]
+        }
+
+    filter{"configurations:Debug64", "system:windows"}
+        libdirs
+        {
+            rootDir .. [[lib/Win64/Debug/]],
+        }
+
+    filter{"configurations:Test64", "system:windows"}
+        libdirs
+        {
+            rootDir .. [[lib/Win64/Release/]]
+        }
+
+    filter{"configurations:Release64", "system:windows"}
+        libdirs
+        {
+            rootDir .. [[lib/Win64/Release/]]
+        }
+    
+    --------------------------------------------------------------------------------------------------
+    -- Linux Library Directories
+    --------------------------------------------------------------------------------------------------
+
+    filter{"configurations:Debug32", "system:linux"}
+        libdirs
+        {
+            rootDir .. [[lib/Lin32/Debug/]],
+        }
+
+    filter{"configurations:Test32", "system:linux"}
+        libdirs
+        {
+            rootDir .. [[lib/Lin32/Release/]]
+        }
+
+    filter{"configurations:Release32", "system:linux"}
+        libdirs
+        {
+            rootDir .. [[lib/Lin32/Release/]]
+        }
+
+    filter{"configurations:Debug64", "system:linux"}
+        libdirs
+        {
+            rootDir .. [[lib/Lin64/Debug/]],
+        }
+
+    filter{"configurations:Test64", "system:linux"}
+        libdirs
+        {
+            rootDir .. [[lib/Lin64/Release/]]
+        }
+
+    filter{"configurations:Release64", "system:linux"}
+        libdirs
+        {
+            rootDir .. [[lib/Lin64/Release/]]
+        }
+
+    filter{}
+end
+
+function libsodium.Link(rootdir, exeliusLibDir)
+
+	filter{"system:windows"}
+		links
+		{
+			"libsodium"
+		}
+
+	filter{"system:linux"}
+		links
+		{
+			"libsodium"
+		}
+
+	filter {}
+end
+
+dependencies.Add("libsodium", libsodium)
